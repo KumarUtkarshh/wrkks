0a2219b7738a592d90d80fadad46fdf4898cc418
devolos <utk27032005@gmail.com>
2026-02-01 18:11:03 +0530

    Add Supabase integration and SyncUser component for user synchronization

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/navigation-menu";
 import { UserIcon } from "lucide-react";
 import SignUpBtn from "./SignUpBtn";
+import SyncUser from "./SyncUser";
 import { ThemeToggle } from "./ThemeToggle";
 import ArrowBigUpDashIcon from "./ui/arrow-big-up-dash-icon";
 import HomeIcon from "./ui/home-icon";
@@ -19,14 +20,18 @@ const navigationLinks = [
 
 export default function NavBar() {
   return (
-    <header className="border-b mt-1 md:px-36 px-4">
-      <div className="flex h-16 items-center justify-between gap-4">
-        <a className="text-primary hover:text-primary/90" href="#">
-          Wrkks
-        </a>
-        {/* Navigation menu */}
-        <NavigationMenu className="pl-8 md:pl-7">
-          <NavigationMenuList className="gap-2">
+    <header className="border-b mt-1 md:px-36 px-4 transition-all">
+      <div className="relative flex h-16 items-center">
+        {/* Left side */}
+        <div className="flex items-center gap-4">
+          <a className="text-primary hover:text-primary/90" href="#">
+            Wrkks
+          </a>
+        </div>
+
+        {/* Center navigation */}
+        <NavigationMenu className="absolute left-1/2 -translate-x-1/2">
+          <NavigationMenuList className="flex gap-4">
             {navigationLinks.map((link, index) => (
               <NavigationMenuItem key={String(index)}>
                 <NavigationMenuLink
@@ -39,9 +44,11 @@ export default function NavBar() {
             ))}
           </NavigationMenuList>
         </NavigationMenu>
+
         {/* Right side */}
-        <div className="flex items-center gap-4">
+        <div className="ml-auto flex items-center gap-4">
           <ThemeToggle />
+          <SyncUser />
           <SignUpBtn />
         </div>
       </div>
