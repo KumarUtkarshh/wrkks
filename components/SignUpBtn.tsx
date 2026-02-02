f00d482933cdd9948f6c38d7585f89845110dbdd
devolos <utk27032005@gmail.com>
2026-02-02 19:11:37 +0530

    mc

--- components/SignUpBtn.tsx
+++ components/SignUpBtn.tsx
@@ -1,25 +1,34 @@
-import { SignedIn, SignedOut, SignUpButton, UserButton } from "@clerk/nextjs";
-import { Suspense } from "react";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
 export default function SignUpBtn() {
   return (
     <div>
-      <SignedOut>
-        <SignUpButton>
-          <Button
-            variant="outline"
-            className="rounded-xl shadow-sm tracking-wide"
-          >
-            Sign Up
-          </Button>
-        </SignUpButton>
-      </SignedOut>
-      <Suspense fallback={<div>hello</div>}>
+      <ClerkLoading>
+        <div className="size-7.5 rounded-full bg-muted animate-pulse" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedOut>
+          <SignUpButton>
+            <Button
+              variant="outline"
+              className="rounded-xl shadow-sm tracking-wide"
+            >
+              Sign Up
+            </Button>
+          </SignUpButton>
+        </SignedOut>
         <SignedIn>
           <UserButton />
         </SignedIn>
-      </Suspense>
+      </ClerkLoaded>
     </div>
   );
 }
